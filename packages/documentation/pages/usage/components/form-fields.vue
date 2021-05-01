1737201dbb37b1525b24277eea9eb0b5051f0d4a
Moritz Vetter <16950410+HansAuger@users.noreply.github.com>
2021-05-01 18:13:48 +0200

    fix(documentation): remove props that pass null to generic component in form-fields documentation page

--- packages/documentation/pages/usage/components/form-fields.vue
+++ packages/documentation/pages/usage/components/form-fields.vue
@@ -322,6 +322,7 @@ import {
 	ComponentValue,
 	ComponentNames,
 	generateComponentCode,
+	omitNullValues,
 } from '../../utilities'
 
 import ComponentInfo from '~/components/ComponentInfo.vue'
@@ -766,7 +767,7 @@ export default defineComponent({
 			(): ComponentValue => ({
 				hasHelpTextSlot: settings.value.hasHelpTextSlot,
 				name: settings.value.component,
-				props: cloneDeep(componentProps.value),
+				props: omitNullValues(cloneDeep(componentProps.value)),
 				validation: settings.value.validation,
 			}),
 		)
