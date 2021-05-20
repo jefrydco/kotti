fd9fa58270a56f3f8db7b52846870652fbc5db16
Moritz Vetter <16950410+HansAuger@users.noreply.github.com>
2021-05-20 21:18:15 +0200

    feat(#439): add interactivity and position to KtLine

--- packages/kotti-ui/source/kotti-line/KtLine.vue
+++ packages/kotti-ui/source/kotti-line/KtLine.vue
@@ -1,23 +1,46 @@
 <template>
 	<div class="kt-line">
-		<div class="kt-line__line" />
-		<div v-if="text" class="kt-line__text" v-text="text" />
-		<div v-if="text" class="kt-line__line" />
+		<div v-if="position !== Position.LEFT" class="kt-line__line" />
+		<div v-if="text" :class="textClasses" @click="handleClick" v-text="text" />
+		<div v-if="position !== Position.RIGHT" class="kt-line__line" />
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { computed, defineComponent } from '@vue/composition-api'
 
 import { KottiLine } from './types'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const validatePosition = (value: any): value is KottiLine.Position =>
+	[
+		KottiLine.Position.LEFT,
+		KottiLine.Position.CENTER,
+		KottiLine.Position.RIGHT,
+	].includes(value)
 
 export default defineComponent<KottiLine.InternalProps>({
 	name: 'KtLine',
 	props: {
-		text: {
+		isInteractive: { type: Boolean, default: false },
+		position: {
 			type: String,
-			default: null,
+			default: KottiLine.Position.CENTER,
+			validator: validatePosition,
 		},
+		text: { type: String, default: null },
+	},
+	setup(props, { emit }) {
+		return {
+			handleClick: () => {
+				if (props.isInteractive) emit('click')
+			},
+			textClasses: computed(() => ({
+				'kt-line__text': true,
+				'kt-line__text--is-interactive': props.isInteractive,
+			})),
+			Position: KottiLine.Position,
+		}
 	},
 })
 </script>
@@ -33,15 +56,21 @@ export default defineComponent<KottiLine.InternalProps>({
 		flex: 1 1 auto;
 		width: 100%;
 		height: 1px;
-		background: var(--gray-30);
+		background: var(--ui-02);
 	}
 
 	&__text {
 		flex: 1 0 auto;
 		padding: 0 0.4rem;
 		font-size: 0.6rem;
-		color: var(--gray-30);
+		color: var(--text-03);
 		text-align: center;
+
+		&--is-interactive {
+			font-size: 0.8rem;
+			color: var(--interactive-01);
+			cursor: pointer;
+		}
 	}
 }
 </style>
--- packages/kotti-ui/source/kotti-line/types.ts
+++ packages/kotti-ui/source/kotti-line/types.ts
@@ -1,7 +1,14 @@
 import { SpecifyRequiredProps } from '../types/utilities'
 
 export namespace KottiLine {
+	export enum Position {
+		LEFT = 'left',
+		CENTER = 'center',
+		RIGHT = 'right',
+	}
 	export type InternalProps = {
+		isInteractive: boolean
+		position: Position
 		text: string
 	}
 
